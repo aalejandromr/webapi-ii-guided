e8a11a13ff0d2fcc89aeb8ece4a4abe5e0d13c14
Alejandro Mejia <aalejandro.mreyes@icloud.com>
2019-08-22 21:47:49 -0400

    Adds router

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const HubsRouter = require("./hubs-router");
+
+const server = express();
+
+server.use(express.json()); // middleware
+
+server.use("/api/hubs", HubsRouter); // base url and hook the router
+
+server.get("/", (req, res) => {
+  res.send(`
+    <h2>Lambda Hubs API</h>
+    <p>Welcome to the Lambda Hubs API</p>
+  `);
+});
+
+// this is how you export so the others can use it
+module.exports = server;
